fb46f8dcd5b46733f1b427de86f01326f843b892
João Pedro Torres <jaopedro05@outlook.com>
2020-03-15 18:02:39 -0300

    cleanup

--- Source/ModBaseBurnItForFuel.cs
+++ Source/ModBaseBurnItForFuel.cs
@@ -1,10 +1,8 @@
-﻿using HarmonyLib;
-using HugsLib;
+﻿using HugsLib;
 using HugsLib.Settings;
 using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEngine;
 using Verse;
 
