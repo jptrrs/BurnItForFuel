3f4efc22755978c384fd922a1d0b98133492132d
jptrrs <jaopedro@gmail.com>
2025-07-19 03:01:28 -0300

    Fixed the tree filter not working on the mod settings panel outside of a running game. Cleaned up some code.

--- Source/HarmonyPatch.cs
+++ Source/HarmonyPatch.cs
@@ -22,96 +22,54 @@ namespace BurnItForFuel
             //Harmony.DEBUG = true;
             var harmonyInstance = new Harmony("JPT_BurnItForFuel");
 
+            //only for DEBUG purposes:
             harmonyInstance.Patch(original: AccessTools.Method(type: typeof(RefuelWorkGiverUtility), name: "CanRefuel"),
                 prefix: null, postfix: new HarmonyMethod(patchType, nameof(CanRefuel_Postfix)), transpiler: null);
 
-            //harmonyInstance.Patch(original: AccessTools.Method(type: typeof(RefuelWorkGiverUtility), name: "FindBestFuel"),
-            //    prefix: null, postfix: new HarmonyMethod(patchType, nameof(FindBestFuel_Postfix)), transpiler: null);
-
+            //Our own way of looking for fuels.
             harmonyInstance.Patch(AccessTools.Method(typeof(RefuelWorkGiverUtility), "FindAllFuel"),
                 new HarmonyMethod(patchType, nameof(FindAllFuel_Prefix)));
-
-            //harmonyInstance.Patch(AccessTools.Method(typeof(CompRefuelable), "GetFuelCountToFullyRefuel"),
-            //    new HarmonyMethod(patchType, nameof(GetFuelCountToFullyRefuel_Prefix)));
-
-            //test
-            //harmonyInstance.Patch(AccessTools.Method(typeof(RefuelWorkGiverUtility), "FindAllFuel"),
-            //    null, new HarmonyMethod(patchType, nameof(FindAllFuel_Postfix)), new HarmonyMethod(patchType, nameof(FuelFilter_Transpiler)));
             harmonyInstance.Patch(AccessTools.Method(typeof(RefuelWorkGiverUtility), "FindBestFuel"),
                 null, new HarmonyMethod(patchType, nameof(FindBestFuel_Postfix)), new HarmonyMethod(patchType, nameof(FuelFilter_Transpiler)));
 
+            //Teaching the tree filter thingy to behave when employed on the mod settings panel outside of a running game. 
+            harmonyInstance.Patch(AccessTools.Method(typeof(Listing_TreeThingFilter), nameof(Listing_TreeThingFilter.DoCategoryChildren)),
+                null, null, new HarmonyMethod(patchType, nameof(HiddenItemsManager_Transpiler)));
+            harmonyInstance.Patch(AccessTools.Method(typeof(QuickSearchFilter), nameof(QuickSearchFilter.Matches), new Type[] {typeof(ThingDef)}),
+                null, null, new HarmonyMethod(patchType, nameof(HiddenItemsManager_Transpiler)));
         }
 
-        //public static bool CompRefuelable_Props_Prefix(CompRefuelable __instance, ref CompProperties __result)
-        //{
-        //    var compSelectFuel = __instance.parent.TryGetComp<CompSelectFuel>();
-        //    if (compSelectFuel != null)
-        //    {
-        //        __result = compSelectFuel.Props;
-        //        return false;
-        //    }
-        //    return true;
-        //}
+        //Replaces Find.HiddenItemsManager.Hidden(ThingDef) with HiddenItemsManager_bypass(ThingDef) on the targeted methods.
+        static IEnumerable<CodeInstruction> HiddenItemsManager_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) 
+        {
+            List<CodeInstruction> code = instructions.ToList();
+            for (int i = 0; i < code.Count; i++)
+            {
+                if (code[i].opcode == OpCodes.Callvirt && (MethodInfo)code[i].operand == AccessTools.Method(typeof(HiddenItemsManager), "Hidden"))
+                {
+                    code.RemoveAt(i);
+                    code.Insert(i, new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(HarmonyPatches), nameof(HiddenItemsManager_Bypass), new Type[] { typeof(ThingDef) }))); 
+                    code.RemoveAt(i - 2);
+                    break;
+                }
+            }
+            foreach (var c in code) yield return c;
+        }
+
+        public static bool HiddenItemsManager_Bypass(ThingDef t)
+        {
+            return Current.ProgramState == ProgramState.Playing ? Find.HiddenItemsManager.Hidden(t) : false;
+        }
 
         public static void CanRefuel_Postfix(object __instance, Pawn pawn, Thing t, bool forced, ref bool __result)
         {
             Log.Message($"[BurnItForFuel] CanRefuel? {__result.ToStringYesNo()}.");
         }
 
-        //public static bool CanRefuel(Pawn pawn, Thing t, bool forced = false)
-        //{
-        //    CompRefuelable compRefuelable = t.TryGetComp<CompRefuelable>();
-        //    if (compRefuelable == null || compRefuelable.IsFull || (!forced && !compRefuelable.allowAutoRefuel))
-        //    {
-        //        return false;
-        //    }
-        //    if (!forced && !compRefuelable.ShouldAutoRefuelNow)
-        //    {
-        //        return false;
-        //    }
-        //    if (t.IsForbidden(pawn) || !pawn.CanReserve(t, 1, -1, null, forced))
-        //    {
-        //        return false;
-        //    }
-        //    if (t.Faction != pawn.Faction)
-        //    {
-        //        return false;
-        //    }
-        //    ThingFilter fuelFilter = new ThingFilter();
-        //    CompSelectFuel comp = t.TryGetComp<CompSelectFuel>();
-        //    comp.PurgeFuelSettings(); //Can we improve this so it's called only when necessary?
-        //    fuelFilter = comp.FuelSettings.filter;
-        //    if (RefuelWorkGiverUtility.FindBestFuel(pawn, t) == null) //diverted to our own.
-        //    {
-        //        JobFailReason.Is("NoFuelToRefuel".Translate(fuelFilter.Summary), null);
-        //        return false;
-        //    }
-        //    if (t.TryGetComp<CompRefuelable>().Props.atomicFueling && FindAllFuel(pawn, t) == null) //diverted to our own.
-        //    {
-        //        JobFailReason.Is("NoFuelToRefuel".Translate(fuelFilter.Summary), null);
-        //        return false;
-        //    }
-        //    return true;
-        //}
-
         public static void FindBestFuel_Postfix(Pawn pawn, Thing refuelable, ref Thing __result)
         {
             Log.Message($"[BurnItForFuel] FindBestFuel returns {__result.Label}. Atomic? {refuelable.TryGetComp<CompRefuelable>().Props.atomicFueling}");
         }
-
-        //private static Thing FindBestFuel(Pawn pawn, Thing refuelable)
-        //{
-        //    ThingFilter filter = new ThingFilter();
-        //    filter = refuelable.TryGetComp<CompSelectFuel>().FuelSettings.filter; //filter diverted to our own.
-        //    Predicate<Thing> predicate = (Thing x) => !x.IsForbidden(pawn) && pawn.CanReserve(x, 1, -1, null, false) && filter.Allows(x);
-        //    IntVec3 position = pawn.Position;
-        //    Map map = pawn.Map;
-        //    ThingRequest bestThingRequest = filter.BestThingRequest;
-        //    PathEndMode peMode = PathEndMode.ClosestTouch;
-        //    TraverseParms traverseParams = TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false);
-        //    Predicate<Thing> validator = predicate;
-        //    return GenClosest.ClosestThingReachable(position, map, bestThingRequest, peMode, traverseParams, 9999f, validator, null, 0, -1, false, RegionType.Set_Passable, false);
-        //}
 
         public static bool FindAllFuel_Prefix(Pawn pawn, Thing refuelable, ref List<Thing> __result, MethodInfo __originalMethod)
         {
@@ -133,74 +91,7 @@ namespace BurnItForFuel
             return result;
         }
 
-        //private static List<Thing> FindAllFuel(Pawn pawn, Thing refuelable)
-        //{
-        //    CompRefuelable comp = refuelable.TryGetComp<CompRefuelable>();
-        //    int quantity = GetFuelCountToFullyRefuel(comp); //diverted to our own.
-        //    ThingFilter filter = new ThingFilter();
-        //    filter = refuelable.TryGetComp<CompSelectFuel>().FuelSettings.filter; //filter diverted to our own.
-
-        //    //from here, doing the work of FindEnoughReservableThings:
-        //    Predicate<Thing> validator = (Thing x) => !x.IsForbidden(pawn) && pawn.CanReserve(x, 1, -1, null, false) && filter.Allows(x);
-        //    IntVec3 position = refuelable.Position;
-        //    Region region = position.GetRegion(pawn.Map, RegionType.Set_Passable); // NOTE: comes out null if refuelable is inside a wall, even with matching RegionType. Why?
-        //    TraverseParms traverseParams = TraverseParms.For(pawn, Danger.Deadly, TraverseMode.ByPawn, false);
-        //    RegionEntryPredicate entryCondition = (Region from, Region r) => r.Allows(traverseParams, false);
-        //    List<Thing> chosenThings = new List<Thing>();
-        //    int accumulatedQuantity = 0;
-        //    RegionProcessor regionProcessor = delegate (Region r)
-        //    {
-        //        List<Thing> list = r.ListerThings.ThingsMatching(ThingRequest.ForGroup(ThingRequestGroup.HaulableEver));
-        //        for (int i = 0; i < list.Count; i++)
-        //        {
-        //            Thing thing = list[i];
-        //            if (validator(thing) && !chosenThings.Contains(thing) && ReachabilityWithinRegion.ThingFromRegionListerReachable(thing, r, PathEndMode.ClosestTouch, pawn))
-        //            {
-        //                chosenThings.Add(thing);
-        //                accumulatedQuantity += thing.stackCount;
-        //                if (accumulatedQuantity >= quantity) return true;
-        //            }
-        //        }
-        //        return false;
-        //    };
-        //    RegionTraverser.BreadthFirstTraverse(region, entryCondition, regionProcessor, 99999, RegionType.Set_Passable);
-        //    if (accumulatedQuantity >= quantity) return chosenThings;
-        //    return null;
-        //}
-
-        //public static bool GetFuelCountToFullyRefuel_Prefix(CompRefuelable __instance, ref int __result)
-        //{
-        //    __result = GetFuelCountToFullyRefuel(__instance);
-        //    return false;
-        //}
-
-        //public static int GetFuelCountToFullyRefuel(CompRefuelable __instance) //skips measures for "atomicFueling" -- This is were we need to input fuel power calculation.
-        //{
-        //    float f = (__instance.TargetFuelLevel - __instance.Fuel) / __instance.Props.FuelMultiplierCurrentDifficulty;
-        //    return Mathf.Max(Mathf.CeilToInt(f), 1);
-        //}
-
-        static IEnumerable<CodeInstruction> FuelFilter_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
-        {
-            List<CodeInstruction> code = instructions.ToList();
-            for (int i = 0; i < code.Count; i++)
-            {
-                if (code[i].opcode == OpCodes.Callvirt && (MethodInfo)code[i].operand == AccessTools.PropertyGetter(typeof(CompRefuelable), "Props"))
-                {
-                    code.RemoveAt(i - 1); // TryGetComp<CompRefuelable>()
-                    code.RemoveAt(i - 1); // .Props
-                    code.RemoveAt(i - 1); // .fuelFilter
-                    code.Insert(i - 1, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ThingCompUtility), "TryGetComp", new Type[] { typeof(Thing) }, new Type[] { typeof(CompSelectFuel) }))); // TryGetComp<CompSelectFuel>()
-                    code.Insert(i, new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(CompSelectFuel), nameof(CompSelectFuel.FuelSettings)))); // .FuelSettings
-                    code.Insert(i + 1, new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(StorageSettings), nameof(StorageSettings.filter)))); // .filter
-                    break;
-                }
-            }
-            foreach (var c in code) yield return c;
-        }
-
-        //Ideia:
-        //Já que GetFuelCountToFullyRefuel retorna a quantidade de combustivel padrão, podemos fazer a equivalência com outros combustíveis no momento em que a busca é feita, quando o item potencial é averiguado. Aparentemente, essa é a função de FindEnoughReservableThings. ThingListProcessor (nested) acumula o stackcount de cada item e compara com a desiredQuantity, que por sua vez se refere ao combustível padraõ.
+        //Ideia: Já que GetFuelCountToFullyRefuel retorna a quantidade de combustivel padrão, podemos fazer a equivalência com outros combustíveis no momento em que a busca é feita, quando o item potencial é averiguado. Aparentemente, essa é a função de FindEnoughReservableThings. ThingListProcessor (nested) acumula o stackcount de cada item e compara com a desiredQuantity, que por sua vez se refere ao combustível padrão.
         public static List<Thing> FindEnoughReservableThings(Pawn pawn, IntVec3 rootCell, IntRange desiredQuantity, CompSelectFuel compSelectFuel)
         {
             Region region2 = rootCell.GetRegion(pawn.Map);
@@ -259,6 +150,26 @@ namespace BurnItForFuel
 
                 return true;
             }
+        }
+
+        //Replaces TryGetComp<CompRefuelable>().Props.fuelFilter with TryGetComp<CompSelectFuel>().FuelSettings.filter.
+        static IEnumerable<CodeInstruction> FuelFilter_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            List<CodeInstruction> code = instructions.ToList();
+            for (int i = 0; i < code.Count; i++)
+            {
+                if (code[i].opcode == OpCodes.Callvirt && (MethodInfo)code[i].operand == AccessTools.PropertyGetter(typeof(CompRefuelable), "Props"))
+                {
+                    code.RemoveAt(i - 1); //TryGetComp<CompRefuelable>()
+                    code.RemoveAt(i - 1); //.Props
+                    code.RemoveAt(i - 1); //.fuelFilter
+                    code.Insert(i - 1, new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ThingCompUtility), "TryGetComp", new Type[] { typeof(Thing) }, new Type[] { typeof(CompSelectFuel) }))); //TryGetComp<CompSelectFuel>()
+                    code.Insert(i, new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(CompSelectFuel), nameof(CompSelectFuel.FuelSettings)))); //.FuelSettings
+                    code.Insert(i + 1, new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(StorageSettings), nameof(StorageSettings.filter)))); //.filter
+                    break;
+                }
+            }
+            foreach (var c in code) yield return c;
         }
 
     }
--- Source/SettingsUI.cs
+++ Source/SettingsUI.cs
@@ -16,35 +16,39 @@ namespace BurnItForFuel
         public static bool CustomDrawer_ThingFilter(Rect rect, ref ThingFilter filter, ThingFilter parentfilter/*, ThingFilter defaultFilter, SettingHandle<FuelSettingsHandle> fuels*/)
         {
             bool test = filter != null;
-            Log.Message($"CustomDrawer_ThingFilter called with rect: {rect} and filter is {test}");
+            //Log.Message($"CustomDrawer_ThingFilter called with rect: {rect} and filter is {test}");
             if (!test) return false;
             Rect labelRect = new Rect(rect);
             labelRect.width -= 20f;
             labelRect.position = new Vector2(labelRect.position.x - rect.width, labelRect.position.y);
             Text.Anchor = TextAnchor.UpperLeft;
-            if (Current.ProgramState != ProgramState.Playing)
-            {
-                DoThingFilterConfigWindow(rect, thingFilterState, ref filter, parentfilter);
-                //Widgets.Label(labelRect, "Start or load a game.".Translate());
-            }
-            else
-            {
-                ThingFilterUI.DoThingFilterConfigWindow(rect, thingFilterState, filter, parentfilter, 1, null, DefDatabase<SpecialThingFilterDef>.AllDefs);
-                //Widgets.Label(labelRect, "FuelSettingsNote".Translate());
-                //fuels.HasUnsavedChanges = true;
-            }
+            //if (Current.ProgramState != ProgramState.Playing)
+            //{
+            //    DoThingFilterConfigWindow(rect, thingFilterState, ref filter, parentfilter);
+            //    //Widgets.Label(labelRect, "Start or load a game.".Translate());
+            //}
+            //else
+            //{
+            //    ThingFilterUI.DoThingFilterConfigWindow(rect, thingFilterState, filter, parentfilter, 1, null, DefDatabase<SpecialThingFilterDef>.AllDefs);
+            //    //Widgets.Label(labelRect, "FuelSettingsNote".Translate());
+            //    //fuels.HasUnsavedChanges = true;
+            //}
+
+            //Testing using vanilla no matter the state
+            ThingFilterUI.DoThingFilterConfigWindow(rect, thingFilterState, filter, parentfilter, 1, null, DefDatabase<SpecialThingFilterDef>.AllDefs);
+
             return true;
         }
 
         public static void DoThingFilterConfigWindow(Rect rect, ThingFilterUI.UIState state, ref ThingFilter filter, ThingFilter parentFilter = null, ThingFilter defaultFilter = null, int openMask = 1)
         {
             Widgets.DrawMenuSection(rect);
-            Text.Font = GameFont.Tiny;
+            Text.Font = GameFont.Tiny; // no font on original
             float num = rect.width - 2f;
             Rect rect2 = new Rect(rect.x + 1f, rect.y + 1f, num / 2f, 24f);
             if (Widgets.ButtonText(rect2, "ClearAll".Translate(), true, true, true))
             {
-                filter.SetDisallowAll(null, null);
+                filter.SetDisallowAll(null, null); //original takes forceHiddenDefs, forceHiddenFilters
                 SoundDefOf.Checkbox_TurnedOff.PlayOneShotOnCamera(null);
             }
             if (Widgets.ButtonText(new Rect(rect2.xMax + 1f, rect2.y, rect.xMax - 1f - (rect2.xMax + 1f), 24f), "AllowAll".Translate(), true, true, true))
@@ -52,18 +56,20 @@ namespace BurnItForFuel
                 filter.SetAllowAll(parentFilter, false);
                 SoundDefOf.Checkbox_TurnedOn.PlayOneShotOnCamera(null);
             }
-            Text.Font = GameFont.Small;
+            Text.Font = GameFont.Small; // no font on original
             rect.yMin = rect2.yMax;
             int num2 = 1;
-            Rect rect3 = new Rect(rect.x + 1f, rect.y + 1f + (float)num2, rect.width - 2f, buttonHeight);
+            Rect rect3 = new Rect(rect.x + 1f, rect.y + 1f + (float)num2, rect.width - 2f, buttonHeight); // different numbers on original
             state.quickSearch.OnGUI(rect3, null);
             rect.yMin = rect3.yMax;
-            rect.height -= buttonHeight + 1f;
+            rect.height -= buttonHeight + 1f; // no button height on original
             TreeNode_ThingCategory node = ThingCategoryNodeDatabase.RootNode;
             if (parentFilter != null)
             {
-                node = parentFilter.DisplayRootCategory;
+                node = parentFilter.DisplayRootCategory; //original also sets other "configurable" fields
             }
+
+            // our interference
             node.catDef.ResolveReferences();
 
             if (node.catDef.SortedChildThingDefs.NullOrEmpty())
@@ -71,19 +77,22 @@ namespace BurnItForFuel
                 Log.Message($"{node.catDef.defName} has no children");
                 return;
             }
+            // end of our interference
 
 
             Rect viewRect = new Rect(0f, 0f, rect.width - 16f, viewHeight);
             Rect visibleRect = new Rect(0f, 0f, rect.width, rect.height);
             visibleRect.position += state.scrollPosition;
-            Widgets.BeginScrollView(rect, ref state.scrollPosition, viewRect, true);
+            Widgets.BeginScrollView(rect, ref state.scrollPosition, viewRect, true); // comes a few lines later on original
+            //on original, a couple of other conditinal configs here
             float y = 2f;
             Rect rect4 = new Rect(0f, y, viewRect.width, 9999f);
             visibleRect.position -= rect4.position;
+
             Listing_TreeThingFilter listing_TreeThingFilter = new Listing_TreeThingFilter(filter, parentFilter, null, DefDatabase<SpecialThingFilterDef>.AllDefs, null, state.quickSearch.filter);
             listing_TreeThingFilter.visibleRect = rect4;
             listing_TreeThingFilter.Begin(rect4);
-            DoCategoryChildrenNoMap(listing_TreeThingFilter, node, 0, openMask, new Map(), false);
+            DoCategoryChildrenNoMap(listing_TreeThingFilter, node, 0, openMask, new Map(), false); // on original, "ListCategorChildren" here
             listing_TreeThingFilter.End();
             state.quickSearch.noResultsMatched = (listing_TreeThingFilter.matchCount == 0);
             if (Event.current.type == EventType.Layout)
@@ -91,6 +100,8 @@ namespace BurnItForFuel
                 viewHeight = y + listing_TreeThingFilter.CurHeight + 90f;
             }
             Widgets.EndScrollView();
+
+            // Our Reset button
             Rect buttonRect = new Rect(rect.x + 1f, rect.yMax + 1f, num, buttonHeight);
             buttonRect.height = 24f;
             buttonRect.position = new Vector2(buttonRect.position.x, rect.yMax);
@@ -104,7 +115,7 @@ namespace BurnItForFuel
         private static float viewHeight;
         private const float buttonHeight = 24f;
 
-        private static void DoCategoryChildrenNoMap(Listing_TreeThingFilter tree, TreeNode_ThingCategory node, int indentLevel, int openMask, Map map, bool subtreeMatchedSearch)
+        public static void DoCategoryChildrenNoMap(Listing_TreeThingFilter tree, TreeNode_ThingCategory node, int indentLevel, int openMask, Map map, bool subtreeMatchedSearch)
         {
             int step = 0;
             List<SpecialThingFilterDef> childSpecialFilters = node.catDef.childSpecialFilters;
@@ -116,7 +127,7 @@ namespace BurnItForFuel
                 }
             }
             step++;
-            Log.Message($"STEP {step}");
+            Log.Message($"STEP {step}"); //1
 
             foreach (TreeNode_ThingCategory childCategoryNode in node.ChildCategoryNodes)
             {
@@ -126,20 +137,24 @@ namespace BurnItForFuel
                 }
             }
             step++;
-            Log.Message($"STEP {step}: {!node.catDef.SortedChildThingDefs.NullOrEmpty()} ");
+            Log.Message($"STEP {step}: sortedChildThingDef? {!node.catDef.SortedChildThingDefs.NullOrEmpty()}, HiddenItemsManager? {Find.HiddenItemsManager != null}"); //2
 
             List<ThingDef> list = new List<ThingDef>();
             foreach (ThingDef sortedChildThingDef in node.catDef.SortedChildThingDefs)
             {
-                DoThingDefNoMap(tree, sortedChildThingDef, indentLevel);
-                //if (Find.HiddenItemsManager.Hidden(sortedChildThingDef))
-                //{
-                //    list.Add(sortedChildThingDef);
-                //}
-                //else if (tree.Visible(sortedChildThingDef) && !HideThingDueToSearch(sortedChildThingDef))
-                //{
-                //    DoThingDefNoMap(tree, sortedChildThingDef, indentLevel);
-                //}
+                Log.Message($"checking {sortedChildThingDef.defName} in {node.catDef.defName}.");
+                //DoThingDefNoMap(tree, sortedChildThingDef, indentLevel);
+                if (Find.HiddenItemsManager.Hidden(sortedChildThingDef))
+                {
+                    Log.Message("case A");
+                    list.Add(sortedChildThingDef);
+                }
+                else if (tree.Visible(sortedChildThingDef) && !HideThingDueToSearch(sortedChildThingDef))
+                {
+                    Log.Message("case B");
+                    tree.DoThingDef(sortedChildThingDef, indentLevel, map);
+                    //DoThingDefNoMap(tree, sortedChildThingDef, indentLevel);
+                }
             }
             step++;
             Log.Message($"STEP {step}");
